dc9248a9367126e04da282023b3a14fafd2a60b5
Berk <berk1937@gmail.com>
2022-12-26 22:01:13 +0300

    Password store added

--- client/src/main.js
+++ client/src/main.js
@@ -1,3 +1,4 @@
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
@@ -5,7 +6,9 @@ import { loadFonts } from './plugins/webfontloader';
 
 loadFonts();
 
+const pinia = createPinia();
 const app = createApp(App);
 
 app.use(vuetify);
+app.use(pinia);
 app.mount('#app');
